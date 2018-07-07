895a0931b55118444e578797ab31a4b58d2c8dba
Artyom <artemij163@gmail.com>
2018-07-07 19:32:57 +0400

    Мой третий коммит

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,30 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+
 import styled from 'styled-components';
+
+import Header from './components/header'
+import Main from './components/main'
+import Menu from './components/menu'
+
+const url = process.env.PUBLIC_URL + '/img/bg.png';
+
+const HeaderWrapper = styled.header`
+  width:100%;
+  height:49px;
+  background-color:#242424;
+  opacity:0.8;
+`
+const MenuWrapper = styled.div`
+  height:89px;
+  padding-top:20px;
+`
+const MainWrapper = styled.main`
+  height:600px;
+  padding-top:130px;
+  background:url(${url}) no-repeat;
+  background-size:cover;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="Wrapper">
-      <SayFulName name="Artyom" surname="Furashov" link="vk.com"/>
-      <SayFulName name="Adel" surname="Furashova" link="vk.com"/>
-      <SayFulName name="Art" surname="Fur" link="vk.com"/>
-    </div>
+      <div className="App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper> 
+          <Grid> 
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+          <Grid> 
+            <Main />
+          </Grid>
+        </MainWrapper>
+      </div>  
     );
   }
-}
-
-function SayFulName(props){
-  return(
-    <div>
-    <h1> Моё имя {props.name}, фамилия {props.surname}</h1>
-    <a href={props.link}> Ссылка на мой профиль </a>
-    </div>
-    )
 }
 
 export default App;
---
+++ slider/src/components/main.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+const Repair = styled.div`
+	color:#ffffff;
+	font-size:40px;
+	font-weight:700;
+	line-height:50px;
+
+	span {
+		display:block;
+		font-size:24px;
+		line-height:30px;
+	}
+`
+
+const LightText = styled.div`
+	color:#ffffff;
+	font-size:18px;
+	font-weight:300;
+	line-height:28px;
+	margin-top:54px;
+`
+const Callbtn = styled.button`
+	width:247;
+	height:67;
+	background-color:#ffa14b;
+	border-radius:30px
+	margin-top:32px;
+	color:#ffffff;
+	font-size:18px;
+	font-weight:bold;
+`
+
+class Main extends React.Component {
+	render(){
+		return (
+			<Row>
+				<Col lg={5}>
+				<Repair> Качественный ремонт
+					<span> iphone за 35 минут и гарантия 1 год </span>
+				</Repair>
+				<LightText> Оставьте заявку на бесплатную диагностику без очереди,
+					и получите защитное стекло, стоимостью 1000 рублей, 
+					с установкой в подарок! 
+				</LightText>
+				<Callbtn> Отправить заявку!
+				</Callbtn>
+				</Col>
+				<Col lg={6} lgOfset={1}>
+
+				</Col>
+			</Row>
+			)
+	}
+}
+
+export default Main
